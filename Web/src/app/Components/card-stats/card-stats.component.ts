9b8b3f06e07fc0f26234ff617e5678942b186fb6
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-03-29 20:36:56 +0200

    card-stats

---
+++ Web/src/app/Components/card-stats/card-stats.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-stats',
+  templateUrl: './card-stats.component.html',
+  styleUrls: ['./card-stats.component.scss']
+})
+export class CardStatsComponent {
+
+}
--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { CardLoginComponent } from './Pages/LogIn/card-login/card-login.component';
+import { CardStatsComponent } from './Components/card-stats/card-stats.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, CardLoginComponent],
+  declarations: [AppComponent, SidebarComponent, CardLoginComponent, CardStatsComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
