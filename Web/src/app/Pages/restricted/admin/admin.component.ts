6c326e5cb12ce3d908f4799ab7a2f6d9ae54d1de
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-01 21:48:10 +0200

    🧹📄

--- Web/src/app/Pages/restricted/admin/admin.component.ts
+++ Web/src/app/Pages/restricted/admin/admin.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -8,37 +7,7 @@ import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
   styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
-  constructor(
-    public titleService: Title,
-    public router: Router,
-    private activatedRoute: ActivatedRoute
-  ) {}
-
-  ngOnInit() {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        const title = this.getTitle(
-          this.router.routerState,
-          this.router.routerState.root
-        ).join('-');
-        this.titleService.setTitle(title);
-      }
-    });
-  }
-
-  getTitle(state: any, parent: any): string[] {
-    const data: string[] = [];
-    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
-      data.push(parent.snapshot.data.title);
-    }
-
-    if (state && parent) {
-      data.push(...this.getTitle(state, state.firstChild(parent)));
-    }
-    const currentUrl = this.activatedRoute.snapshot.url.join('/');
-    data.push(currentUrl);
-    return data;
-  }
+  constructor(public router: Router) {}
 
   getTitle2(): string {
     let title: string = this.router.url; //=> /admin/dashboard
