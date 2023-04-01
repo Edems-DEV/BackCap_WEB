354fba7380be59b6890c97317c0360cbabc818dc
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-01 20:44:07 +0200

    🆕 Child-router

--- Web/src/app/app-routing.module.ts
+++ Web/src/app/app-routing.module.ts
@@ -6,21 +6,30 @@ import { PageMachinesComponent } from './Pages/restricted/Machines/page-machines
 import { PageJobsComponent } from './Pages/restricted/Jobs/page-jobs/page-jobs.component';
 import { PageUsersComponent } from './Pages/restricted/Users/page-users/page-users.component';
 import { PageLoginComponent } from './Pages/LogIn/page-login/page-login.component';
+import { AdminComponent } from './Pages/restricted/admin/admin.component';
 
+// prettier-ignore
 const routes: Routes = [
   //[Unrestricted]
   { path: 'login', component: PageLoginComponent },
   //[Restricted]
-  //Admin
-  { path: 'dashboard', component: PageDashboardComponent },
-  { path: 'groups', component: PageGroupsComponent },
-  { path: 'machines', component: PageMachinesComponent },
-  { path: 'jobs', component: PageJobsComponent },
-  { path: 'users', component: PageUsersComponent },
-  { path: 'configs', component: PageUsersComponent },
+  {
+    path: 'admin',
+    title: 'First component',
+    component: AdminComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { title: 'dashboard',path: 'dashboard',component: PageDashboardComponent,},
+      { title: 'groups', path: 'groups', component: PageGroupsComponent },
+      { title: 'machines', path: 'machines', component: PageMachinesComponent },
+      { title: 'jobs', path: 'jobs', component: PageJobsComponent },
+      { title: 'users', path: 'users', component: PageUsersComponent },
+      { title: 'configs', path: 'configs', component: PageUsersComponent },
+    ],
+  },
   //WildCards
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'admin/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'admin/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
