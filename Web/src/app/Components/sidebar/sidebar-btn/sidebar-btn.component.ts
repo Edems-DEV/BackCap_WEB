13b1e3b0c51471c2d3562fead4bd85ad8ef7d60d
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-01 14:51:27 +0200

    🧹📄 Sidebar refactor (btn component)

--- Web/src/app/Components/sidebar/sidebar-btn/sidebar-btn.component.ts
+++ Web/src/app/Components/sidebar/sidebar-btn/sidebar-btn.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-btn',
   templateUrl: './sidebar-btn.component.html',
-  styleUrls: ['./sidebar-btn.component.scss']
+  styleUrls: ['./sidebar-btn.component.scss'],
 })
 export class SidebarBtnComponent {
-
+  @Input() title: string | undefined;
+  @Input() icon: string | undefined;
+  @Input() isActive: boolean | undefined;
 }
