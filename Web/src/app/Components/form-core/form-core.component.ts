05abf86203f4eb891ce567d1d0aa5eef0e3a9ffa
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-02 18:04:02 +0200

    🆕 Core form
    - stupid header (R.I.P. 1h)

--- Web/src/app/Components/form-core/form-core.component.ts
+++ Web/src/app/Components/form-core/form-core.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-form-core',
   templateUrl: './form-core.component.html',
-  styleUrls: ['./form-core.component.scss']
+  styleUrls: ['./form-core.component.scss'],
 })
 export class FormCoreComponent {
-
+  //Title
+  @Input() title: string = 'Settings';
+  @Input() icon: string | undefined;
+  @Input() icon2: string | undefined;
+  //Modal
+  constructor(private modalService: NgbModal) {}
+  openVerticallyCentered(content: any) {
+    this.modalService.open(content, { centered: true });
+  }
 }
--- Web/src/app/Pages/restricted/Configs/form-config/form-config.component.ts
+++ Web/src/app/Pages/restricted/Configs/form-config/form-config.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-form-config',
   templateUrl: './form-config.component.html',
   styleUrls: ['./form-config.component.scss'],
 })
-export class FormConfigComponent {
-  constructor(private modalService: NgbModal) {}
-  openVerticallyCentered(content: any) {
-    this.modalService.open(content, { centered: true });
-  }
-}
+export class FormConfigComponent {}
