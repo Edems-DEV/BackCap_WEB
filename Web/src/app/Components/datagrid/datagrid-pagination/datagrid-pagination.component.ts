cdfe7a58aa0a1b8bcb0d18d7dd00c9529113df23
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-02 15:43:35 +0200

    🛠️ Variables (pagination) (0,5)

--- Web/src/app/Components/datagrid/datagrid-pagination/datagrid-pagination.component.ts
+++ Web/src/app/Components/datagrid/datagrid-pagination/datagrid-pagination.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-datagrid-pagination',
   templateUrl: './datagrid-pagination.component.html',
-  styleUrls: ['./datagrid-pagination.component.scss']
+  styleUrls: ['./datagrid-pagination.component.scss'],
 })
 export class DatagridPaginationComponent {
-
+  //@Input() label: string | undefined;
+  //TODO: add dinamic label
+  @Input() offset: number = 1;
+  @Input() limit: number = 20;
+  @Input() total: number = 27;
 }
