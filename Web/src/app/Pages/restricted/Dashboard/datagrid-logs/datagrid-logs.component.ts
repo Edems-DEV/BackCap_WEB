c0d8c177c2005d9b5a1953271db46c4f63861647
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-02 15:59:27 +0200

    🛠️ Datagrid-logs

--- Web/src/app/Pages/restricted/Dashboard/datagrid-logs/datagrid-logs.component.ts
+++ Web/src/app/Pages/restricted/Dashboard/datagrid-logs/datagrid-logs.component.ts
@@ -3,8 +3,77 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-datagrid-logs',
   templateUrl: './datagrid-logs.component.html',
-  styleUrls: ['./datagrid-logs.component.scss']
+  styleUrls: ['./datagrid-logs.component.scss'],
 })
 export class DatagridLogsComponent {
-
+  logs = LOGS;
 }
+
+interface Log {
+  time: Date;
+  status: string;
+  target: string;
+  message: string;
+}
+const LOGS: Log[] = [
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+  {
+    time: new Date('2022-04-30T12:00:00Z'),
+    status: 'First group',
+    target: 'Pc1',
+    message: 'Pc1 - backed up',
+  },
+];
