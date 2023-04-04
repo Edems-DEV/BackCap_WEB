92485104f200c7e3dea09928e31b60c0d0a7625f
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-04 20:12:15 +0200

    🆕👁️ Password EYE (hide/show)

--- Web/src/app/Components/input-my/_basic/input-password/input-password.component.ts
+++ Web/src/app/Components/input-my/_basic/input-password/input-password.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input-password',
   templateUrl: './input-password.component.html',
-  styleUrls: ['./input-password.component.scss']
+  styleUrls: ['./input-password.component.scss'],
 })
 export class InputPasswordComponent {
+  @Input() label: string = '';
+  @Input() value: string = '';
+  @Input() class: string = '';
+  //--------------------------
+  passwordVisible: boolean = false;
 
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
