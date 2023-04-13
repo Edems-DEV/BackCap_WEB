2d0585ffe5cabf507ac0fddf3c011a1fe906638b
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-13 11:31:58 +0200

    🆕 environment
    - API url

--- Web/src/app/services/users.service.ts
+++ Web/src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ import { User } from '../models/user.model';
 export class UsersService {
   constructor(private http: HttpClient) {}
 
-  url: string = 'http://localhost:5056/api/Users';
+  url: string = environment.apiEndpoint + 'users';
 
   public findAll(): Observable<User[]> {
     return this.http.get<User[]>(this.url);
