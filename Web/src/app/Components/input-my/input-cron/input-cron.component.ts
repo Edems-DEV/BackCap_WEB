65765bb046d55ff1afaff989d17ee9646382776a
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-14 11:41:40 +0200

    🛠️ Cron

--- Web/src/app/Components/input-my/input-cron/input-cron.component.ts
+++ Web/src/app/Components/input-my/input-cron/input-cron.component.ts
@@ -1,15 +1,66 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-input-cron',
   templateUrl: './input-cron.component.html',
   styleUrls: ['./input-cron.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputCronComponent),
+      multi: true,
+    },
+  ],
 })
 export class InputCronComponent {
   public isCollapsed = true;
   @Input() label: string = 'Interval';
-  @Input() value: string = '';
 
   active = 1;
+  //-----------------------------------
+  @Input() public parentForm: FormGroup;
+  @Input() public fieldName: string;
+
+  public value: string;
+
+  public changed: (value: string) => void;
+
+  public touched: () => void;
+
+  public isDisabled: boolean;
+
+  get formField(): FormControl {
+    return this.parentForm?.get(this.fieldName) as FormControl;
+  }
+
+  constructor() {}
+
+  public writeValue(value: string): void {
+    this.value = value;
+  }
+
+  public onChange(event: Event): void {
+    const value: string = (<HTMLInputElement>event.target).value;
+
+    this.changed(value);
+  }
+
+  public registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.touched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
 }
