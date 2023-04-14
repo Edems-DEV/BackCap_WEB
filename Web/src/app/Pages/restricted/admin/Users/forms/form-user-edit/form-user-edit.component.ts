77707d5807bde521663c2fee116d54aed8a982fb
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-14 11:47:13 +0200

    🛠️ PUT

--- Web/src/app/Pages/restricted/admin/Users/forms/form-user-edit/form-user-edit.component.ts
+++ Web/src/app/Pages/restricted/admin/Users/forms/form-user-edit/form-user-edit.component.ts
@@ -19,16 +19,10 @@ export class FormUserEditComponent {
 
   public ngOnInit(): void {
     this.form = FormUserComponent.createForm(this.fb, this.user);
-    console.log(this.user); //user is filled with data
   }
 
   public saveUser(values: any): void {
-    /*this.user.name = values.name;
-    this.user.surname = values.surname;
-    this.user.email = values.email;*/
-
     Object.assign(this.user, values);
-    this.service.update(this.user);
-    //.subscribe(() => this.router.navigate(['/users']));
+    this.service.update(this.user).subscribe();
   }
 }
