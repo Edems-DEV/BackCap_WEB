870621f63ce458a377d6e38e87427c4a89a5ef4d
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-15 16:44:26 +0200

    🛠️ stat cards (Machine, Jobs🐞)
    - jobs dont have API yet (TODO)

--- Web/src/app/Pages/restricted/admin/Jobs/stats-jobs/stats-jobs.component.ts
+++ Web/src/app/Pages/restricted/admin/Jobs/stats-jobs/stats-jobs.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { JobsService } from 'src/app/services/jobs/jobs.service';
 
 @Component({
   selector: 'app-stats-jobs',
   templateUrl: './stats-jobs.component.html',
-  styleUrls: ['./stats-jobs.component.scss']
+  styleUrls: ['./stats-jobs.component.scss'],
 })
 export class StatsJobsComponent {
+  jobRunning: number = 0;
+  jobWaiting: number = 0;
+  jobSuccessful: number = 0;
+  jobWarning: number = 0;
+  jobFailed: number = 0;
 
+  constructor(private jobsService: JobsService) {}
+
+  ngOnInit() {
+    this.jobsService.count().subscribe((jobRunning) => {
+      this.jobRunning = jobRunning;
+      console.log(jobRunning);
+    });
+    this.jobsService.count().subscribe((jobWaiting) => {
+      this.jobWaiting = jobWaiting;
+      console.log(jobWaiting);
+    });
+    this.jobsService.count().subscribe((jobSuccessful) => {
+      this.jobSuccessful = jobSuccessful;
+      console.log(jobSuccessful);
+    });
+    this.jobsService.count().subscribe((jobWarning) => {
+      this.jobWarning = jobWarning;
+      console.log(jobWarning);
+    });
+    this.jobsService.count().subscribe((jobFailed) => {
+      this.jobFailed = jobFailed;
+      console.log(jobFailed);
+    });
+    //implement when api is ready
+  }
 }
--- Web/src/app/Pages/restricted/admin/Machines/stats-machines/stats-machines.component.ts
+++ Web/src/app/Pages/restricted/admin/Machines/stats-machines/stats-machines.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { MachinesService } from 'src/app/services/machines/machines.service';
 
 @Component({
   selector: 'app-stats-machines',
   templateUrl: './stats-machines.component.html',
-  styleUrls: ['./stats-machines.component.scss']
+  styleUrls: ['./stats-machines.component.scss'],
 })
 export class StatsMachinesComponent {
+  pcActive: number = 0;
+  pcDisable: number = 0;
+  pcAll: number = 0;
 
+  constructor(private machinesService: MachinesService) {}
+
+  ngOnInit() {
+    this.machinesService.countActive().subscribe((pcActive) => {
+      this.pcActive = pcActive;
+      console.log(pcActive);
+    });
+    this.machinesService.countDisable().subscribe((pcDisable) => {
+      this.pcAll = pcDisable;
+      console.log(pcDisable);
+    });
+    this.machinesService.count().subscribe((pcAll) => {
+      this.pcAll = pcAll;
+      console.log(pcAll);
+    });
+  }
 }
