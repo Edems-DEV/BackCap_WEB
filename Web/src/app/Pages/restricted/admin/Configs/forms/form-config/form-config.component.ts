4daeaea958592217d12c8de9054c23db3a8b7849
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-15 07:28:31 +0200

    🛠️ +BTN form

--- Web/src/app/Pages/restricted/admin/Configs/forms/form-config/form-config.component.ts
+++ Web/src/app/Pages/restricted/admin/Configs/forms/form-config/form-config.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Config } from 'src/app/models/config.model';
 
 @Component({
   selector: 'app-form-config',
   templateUrl: './form-config.component.html',
   styleUrls: ['./form-config.component.scss'],
 })
-export class FormConfigComponent {}
+export class FormConfigComponent {
+  @Input() title: string = 'Create';
+
+  @Input()
+  form: FormGroup;
+
+  @Output()
+  saved: EventEmitter<any> = new EventEmitter<any>();
+
+  public static createForm(fb: FormBuilder, config: Config): FormGroup {
+    return fb.group({
+      type: [config.type, [Validators.required]],
+    });
+  }
+
+  public save(): void {
+    this.saved.emit(this.form.value);
+  }
+}
--- Web/src/app/Pages/restricted/admin/Groups/forms/form-group/form-group.component.ts
+++ Web/src/app/Pages/restricted/admin/Groups/forms/form-group/form-group.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Group } from 'src/app/models/group.model';
 
 @Component({
   selector: 'app-form-group',
   templateUrl: './form-group.component.html',
-  styleUrls: ['./form-group.component.scss']
+  styleUrls: ['./form-group.component.scss'],
 })
 export class FormGroupComponent {
+  @Input() title: string = 'Create';
 
+  @Input()
+  form: FormGroup;
+
+  @Output()
+  saved: EventEmitter<any> = new EventEmitter<any>();
+
+  public static createForm(fb: FormBuilder, group: Group): FormGroup {
+    return fb.group({
+      name: [group.name, [Validators.required]],
+    });
+  }
+
+  public save(): void {
+    this.saved.emit(this.form.value);
+  }
 }
