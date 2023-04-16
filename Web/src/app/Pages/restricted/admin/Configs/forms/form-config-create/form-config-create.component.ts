6b3e397fb30d029818cbd4bd2570dd0de42b174e
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-16 11:01:50 +0200

    💡I can delete constructor?

--- Web/src/app/Pages/restricted/admin/Configs/forms/form-config-create/form-config-create.component.ts
+++ Web/src/app/Pages/restricted/admin/Configs/forms/form-config-create/form-config-create.component.ts
@@ -5,6 +5,13 @@ import { Config } from 'src/app/models/config.model';
 import { ConfigsService } from 'src/app/services/configs/configs.service';
 import { FormConfigComponent } from '../form-config/form-config.component';
 
+import {
+  MachineName,
+  GroupName,
+  Source,
+  Destination,
+} from 'src/app/models/names.model';
+
 @Component({
   selector: 'app-form-config-create',
   templateUrl: './form-config-create.component.html',
@@ -20,12 +27,26 @@ export class FormConfigCreateComponent {
     private service: ConfigsService
   ) {}
 
-  // public ngOnInit(): void {
-  //   this.form = FormConfigComponent.createForm(
-  //     this.fb,
-  //     new Config(0, 0, 0, 0, true, '', '', [''], [''])
-  //   );
-  // }
+  public ngOnInit(): void {
+    this.form = FormConfigComponent.createForm(
+      this.fb,
+      new Config()
+      // 0,
+      // '',
+      // '',
+      // '',
+      // false,
+      // 0,
+      // 0,
+      // '',
+      // new Date(),
+      // new Date(),
+      // new Array<Source>(),
+      // new Array<Destination>(),
+      // new MachineName(0, ''),
+      // new GroupName(0, '')
+    );
+  }
 
   public saveConfig(values: any): void {
     this.service.insert(values).subscribe();
--- Web/src/app/models/config.model.ts
+++ Web/src/app/models/config.model.ts
@@ -14,33 +14,33 @@ export class Config {
   destinations: Destination[];
   machine: MachineName; // => computer's'
   group: GroupName; // => group's'
-  public constructor(
-    id: number,
-    name: string,
-    description: string,
-    type: string,
-    isCompressed: boolean,
-    packageSize: number,
-    retention: number,
-    interval: string,
-    interval_end: Date,
-    sources: Source[],
-    destinations: Destination[],
-    machine: MachineName, //[]
-    group: GroupName // []
-  ) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.type = type;
-    this.isCompressed = isCompressed;
-    this.packageSize = packageSize;
-    this.retention = retention;
-    this.interval = interval;
-    this.interval_end = interval_end;
-    this.sources = sources;
-    this.destinations = destinations;
-    this.machine = machine;
-    this.group = group;
-  }
+  // public constructor(
+  //   id: number,
+  //   name: string,
+  //   description: string,
+  //   type: string,
+  //   isCompressed: boolean,
+  //   packageSize: number,
+  //   retention: number,
+  //   interval: string,
+  //   interval_end: Date,
+  //   sources: Source[],
+  //   destinations: Destination[],
+  //   machine: MachineName, //[]
+  //   group: GroupName // []
+  // ) {
+  //   this.id = id;
+  //   this.name = name;
+  //   this.description = description;
+  //   this.type = type;
+  //   this.isCompressed = isCompressed;
+  //   this.packageSize = packageSize;
+  //   this.retention = retention;
+  //   this.interval = interval;
+  //   this.interval_end = interval_end;
+  //   this.sources = sources;
+  //   this.destinations = destinations;
+  //   this.machine = machine;
+  //   this.group = group;
+  // }
 }
