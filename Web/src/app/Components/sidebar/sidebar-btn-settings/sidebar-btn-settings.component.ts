7086edd32269d35b78578b704657033875bf3050
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-21 09:10:43 +0200

    🛠️ logout

--- Web/src/app/Components/sidebar/sidebar-btn-settings/sidebar-btn-settings.component.ts
+++ Web/src/app/Components/sidebar/sidebar-btn-settings/sidebar-btn-settings.component.ts
@@ -2,16 +2,30 @@ import { Component, Input } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+import { Router } from '@angular/router';
+import { SessionsService } from 'src/app/services/auth/sessions.service';
+
 @Component({
   selector: 'app-sidebar-btn-settings',
   templateUrl: './sidebar-btn-settings.component.html',
   styleUrls: ['./sidebar-btn-settings.component.scss'],
 })
 export class SidebarBtnSettingsComponent {
-  constructor(private modalService: NgbModal) {}
+  @Input() username: string = '?';
+
+  constructor(
+    private modalService: NgbModal,
+    private sessions: SessionsService,
+    private router: Router
+  ) {}
+
   openVerticallyCentered(content: any) {
     this.modalService.open(content, { centered: true });
   }
 
-  @Input() username: string = '?';
+  //Auth
+  public logout(): void {
+    this.sessions.logout();
+    this.router.navigate(['/login']);
+  }
 }
