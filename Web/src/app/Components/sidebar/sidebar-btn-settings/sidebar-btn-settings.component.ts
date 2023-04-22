4afbdd498a01d42ebfd1ff10ef435ca4553d4094
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-22 18:59:14 +0200

    🛠️ Profile edit

--- Web/src/app/Components/sidebar/sidebar-btn-settings/sidebar-btn-settings.component.ts
+++ Web/src/app/Components/sidebar/sidebar-btn-settings/sidebar-btn-settings.component.ts
@@ -5,6 +5,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
 import { SessionsService } from 'src/app/services/auth/sessions.service';
 import { UsersService } from 'src/app/services/users/users.service';
+import { User, User_PSW } from 'src/app/models/user.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar-btn-settings',
@@ -16,19 +18,33 @@ export class SidebarBtnSettingsComponent {
   email: string = 'user1@gmail.com';
   interval: string = '* * * * *';
 
+  user: User_PSW;
+  form: FormGroup;
+
   constructor(
+    private router: Router,
     private modalService: NgbModal,
     private sessions: SessionsService,
-    private router: Router,
-    private userService: UsersService
+    private userService: UsersService,
+    private fb: FormBuilder
   ) {}
 
   public ngOnInit(): void {
     this.userService.findProfile().subscribe((user) => {
-      this.username = user.name;
-      this.email = user.email;
-      this.interval = user.interval_Report;
+      this.user = new User_PSW(user, '');
+      this.form = this.fb.group({
+        name: [user.name, [Validators.required]],
+        email: [user.email, [Validators.required]], //, Validators.email
+        interval_Report: user.interval_Report,
+      });
     });
+  }
+
+  public save(): void {
+    console.log('edit-user: '); //, JSON.stringify(values)
+    Object.assign(this.user, this.form.value);
+    this.userService.update(this.user).subscribe();
+    this.modalService.dismissAll();
   }
 
   openVerticallyCentered(content: any) {
